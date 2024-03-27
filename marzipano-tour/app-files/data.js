7c9580dc40e06c07e68ce68dd701be6356b90cac
linyouxin <amberlin0807@gmail.com>
2024-03-27 14:49:53 +0800

    vr

---
+++ marzipano-tour/app-files/data.js
@@ -0,0 +1,311 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-",
+      "name": "櫃台",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1344,
+      "initialViewParameters": {
+        "yaw": 0.5733596302738313,
+        "pitch": 0.2377580825344907,
+        "fov": 1.2828048894984105
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.7258316918122567,
+          "pitch": 0.25645262484970743,
+          "rotation": 0,
+          "target": "3-"
+        },
+        {
+          "yaw": -1.0670284605859983,
+          "pitch": 0.1801360757236008,
+          "rotation": 0,
+          "target": "2-"
+        },
+        {
+          "yaw": -0.6167850540945867,
+          "pitch": 0.018030178719822487,
+          "rotation": 6.283185307179586,
+          "target": "1-"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 0.8729473596461865,
+          "pitch": -0.026912385813520956,
+          "title": "櫃台",
+          "text": ""
+        },
+        {
+          "yaw": -2.4112901935047724,
+          "pitch": 0.19315148044547925,
+          "title": "門",
+          "text": "door"
+        }
+      ]
+    },
+    {
+      "id": "1-",
+      "name": "觀景台",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1344,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -3.0868780724281333,
+          "pitch": -0.014159297873890253,
+          "rotation": 0,
+          "target": "0-"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -2.78134695421112,
+          "pitch": 0.5701362121548836,
+          "title": "草",
+          "text": ""
+        },
+        {
+          "yaw": 0.3588718909579427,
+          "pitch": -1.3592197313498264,
+          "title": "天空",
+          "text": ""
+        }
+      ]
+    },
+    {
+      "id": "2-",
+      "name": "魚缸",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1625,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.6058241765880465,
+          "pitch": 0.12614700038795235,
+          "rotation": 0,
+          "target": "0-"
+        },
+        {
+          "yaw": -0.6505438268993942,
+          "pitch": 0.2812973018403895,
+          "rotation": 0,
+          "target": "4-"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 0.1475381950230812,
+          "pitch": -0.02542467993504971,
+          "title": "魚",
+          "text": "全長8.8公尺的鯨鯊"
+        },
+        {
+          "yaw": 0.7796870573076369,
+          "pitch": 0.2578399808901892,
+          "title": "小魚",
+          "text": ""
+        }
+      ]
+    },
+    {
+      "id": "3-",
+      "name": "門口",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1024,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.0643628145629318,
+          "pitch": -0.1835498258712036,
+          "rotation": 0,
+          "target": "0-"
+        },
+        {
+          "yaw": -1.50517639967741,
+          "pitch": 0.020025299751351255,
+          "rotation": 0,
+          "target": "5-"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 0.19656806746264976,
+          "pitch": -0.11680646938443573,
+          "title": "樓梯",
+          "text": ""
+        }
+      ]
+    },
+    {
+      "id": "4-",
+      "name": "游泳池",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1024,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.0086889656023459,
+          "pitch": 0.4427034594618551,
+          "rotation": 0,
+          "target": "2-"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 0.041410479616015294,
+          "pitch": 0.6008061479334046,
+          "title": "樓梯",
+          "text": ""
+        }
+      ]
+    },
+    {
+      "id": "5-",
+      "name": "外面",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1024,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.3036827710548078,
+          "pitch": 0.12774535153312883,
+          "rotation": 0,
+          "target": "3-"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 1.8142285736278767,
+          "pitch": -0.11008870521788339,
+          "title": "樹",
+          "text": ""
+        }
+      ]
+    }
+  ],
+  "name": "沖繩美麗海水族館",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": false,
+    "fullscreenButton": true,
+    "viewControlButtons": true
+  }
+};
